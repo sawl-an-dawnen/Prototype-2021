8598eb2a536e629fcfcab844aa92ae962c479a0f
BlazeX15 <jsbahga@hotmail.com>
2024-03-31 16:15:44 -0500

    Sliding puzzle now works correctly and in the canvas

--- Assets/Scripts/Puzzle/SlidingPuzzle.cs
+++ Assets/Scripts/Puzzle/SlidingPuzzle.cs
@@ -14,7 +14,6 @@ public class SlidingPuzzle : MonoBehaviour
     [SerializeField] private Transform Tile6;
     [SerializeField] private Transform Tile7;
     [SerializeField] private Transform Tile8;
-    private Camera cam;
     private AudioSource audioSource;
     public AudioClip puzzleComplete;
     private Vector3 T1;
@@ -26,52 +25,51 @@ public class SlidingPuzzle : MonoBehaviour
     private Vector3 T7;
     private Vector3 T8;
     private bool Won = false;
+    private bool exiting = false;
     float winCheck;
+    public GameObject panel;
     void Start()
     {
-        cam = Camera.main;
-        T1.Set(-20, 50, -1);
-        T2.Set(30, 50, -1);
-        T3.Set(80, 50, -1);
-        T4.Set(-20, 0, -1);
-        T5.Set(30, 0, -1);
-        T6.Set(80, 0, -1);
-        T7.Set(-20, -50, -1);
-        T8.Set(30, -50, -1);
-        //T1.Set((float)-20.24, (float)50.24, (float)-1.00);
-        //T2.Set((float)30.50, (float)50.00, (float)-1.00);
-        //T3.Set((float)80.44, (float)50.08, (float)-1.00);
-        //T4.Set((float)-20.20, (float)0.20, (float)-1.00);
-        //T5.Set((float)30, (float)0, (float)-1);
-        //T6.Set((float)79.69, (float)0.31, (float)-1.00);
-        //T7.Set((float)-20.28, (float)-49.72, (float)-1.00);
-        //T8.Set((float)29.91, (float)-49.31, (float)-1.00);
+        T1.Set(820, 870, -1);
+        T2.Set(1160, 870, -1);
+        T3.Set(1500, 870, -1);
+        T4.Set(820, 530, -1);
+        T5.Set(1160, 530, -1);
+        T6.Set(1500, 530, -1);
+        T7.Set(820, 190, -1);
+        T8.Set(1160, 190, -1);
         winCheck = 0f;
         audioSource = GetComponent<AudioSource>();
+        audioSource.Stop();
     }
 
     
     void Update()
     {
-        winCheck += Time.deltaTime;
-        if (Input.GetMouseButtonDown(0))
+        if (panel.activeSelf && !Won && !exiting)
+		{
+			Pause();
+		}
+        else if (!exiting)
         {
-            Ray pointer = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(pointer.origin, pointer.direction);
-            if (hit && !Won)
-            {
-                if (Vector2.Distance(empty.position,hit.transform.position) < 60)
-                {
-                    audioSource.Stop();
-                    audioSource.Play();
-                    Vector3 lastEmptySpace = empty.position;
-                    TileMovement thisTile = hit.transform.GetComponent<TileMovement>();
-                    empty.position = thisTile.targetPosition;
-                    thisTile.targetPosition = lastEmptySpace;
-                    
-                }
-            }
+            exiting = true;
+            StartCoroutine(returnToGame(2f));
         }
+
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            Tile1.position = T1;
+            Tile2.position = T2;
+            Tile3.position = T3;
+            Tile4.position = T4;
+            Tile5.position = T5;
+            Tile6.position = T6;
+            Tile7.position = T7;
+            Tile8.position = T8;
+            Debug.Log("Cheater found");
+        }
+
+        winCheck += Time.unscaledDeltaTime;
         while (winCheck >= 2)
         {
             CheckFinished();
@@ -81,50 +79,46 @@ public class SlidingPuzzle : MonoBehaviour
 
     void CheckFinished()
     {
-        //Debug.Log("Tile1: " + Tile1.position);
-        //Debug.Log("T1: " + T1);
-        //Debug.Log("Tile2: " + Tile2.position);
-        //Debug.Log("T2: " + T2);
-        //Debug.Log("Tile3: " + Tile3.position);
-        //Debug.Log("T3: " + T3);
-        //Debug.Log("Tile4: " + Tile4.position);
-        //Debug.Log("T4: " + T4);
-        //Debug.Log("Tile5: " + Tile5.position);
-        //Debug.Log("T5: " + T5);
-        //Debug.Log("Tile6: " + Tile6.position);
-        //Debug.Log("T6: " + T6);
-        //Debug.Log("Tile7: " + Tile7.position);
-        //Debug.Log("T7: " + T7);
-        //Debug.Log("Tile8: " + Tile8.position);
-        //Debug.Log("T8: " + T8);
-        //Debug.Log(ComparePositions(Tile1.position, T1));
-        //Debug.Log(ComparePositions(Tile2.position, T2));
-        //Debug.Log(ComparePositions(Tile3.position, T3));
-        //Debug.Log(ComparePositions(Tile4.position, T4));
-        //Debug.Log(ComparePositions(Tile5.position, T5));
-        //Debug.Log(ComparePositions(Tile6.position, T6));
-        //Debug.Log(ComparePositions(Tile7.position, T7));
-        //Debug.Log(ComparePositions(Tile8.position, T8));
-        if (ComparePositions(Tile1.position, T1) && ComparePositions(Tile2.position, T2) && ComparePositions(Tile3.position, T3) && ComparePositions(Tile4.position, T4) && ComparePositions(Tile5.position, T5) && ComparePositions(Tile6.position, T6) && ComparePositions(Tile7.position, T7) && ComparePositions(Tile8.position, T8))
+        Debug.Log(Vector3.Distance(Tile1.position, T1));
+        Debug.Log(Vector3.Distance(Tile2.position, T2));
+        Debug.Log(Vector3.Distance(Tile3.position, T3));
+        Debug.Log(Vector3.Distance(Tile4.position, T4));
+        Debug.Log(Vector3.Distance(Tile5.position, T5));
+        Debug.Log(Vector3.Distance(Tile6.position, T6));
+        Debug.Log(Vector3.Distance(Tile7.position, T7));
+        Debug.Log(Vector3.Distance(Tile8.position, T8));
+
+        if (Vector3.Distance(Tile1.position, T1) < 2 && Vector3.Distance(Tile2.position, T2) < 2 && Vector3.Distance(Tile3.position, T3) < 2 && Vector3.Distance(Tile4.position, T4) < 2 && Vector3.Distance(Tile5.position, T5) < 2 && Vector3.Distance(Tile6.position, T6) < 2 && Vector3.Distance(Tile7.position, T7) < 2 && Vector3.Distance(Tile8.position, T8) < 2 && !Won)
         {
-            //audioSource.Stop();
-            //audioSource.PlayOneShot(puzzleComplete);
+            audioSource.Stop();
+            audioSource.PlayOneShot(puzzleComplete);
             Debug.Log("Win");
             winUI.SetActive(true);  
             Won = true;
         }
     }
 
-    bool ComparePositions(Vector3 A, Vector3 B)
+    public void letTheSoundPlay()
     {
-        if (Mathf.Round(A.x) == B.x && Mathf.Round(A.y) == B.y)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        audioSource.Stop();
+        audioSource.Play();
+    }
 
+    public void Resume()
+	{
+		panel.SetActive(false);
+		Time.timeScale = 1f;
+	}
+
+	public void Pause()
+	{
+		panel.SetActive(true);
+		Time.timeScale = 0f;
+	}
+
+    public IEnumerator returnToGame(float f)
+    {
+        yield return new WaitForSecondsRealtime(f);
+        Resume();
     }
 }
--- Assets/Scripts/Puzzle/TileMovement.cs
+++ Assets/Scripts/Puzzle/TileMovement.cs
@@ -5,14 +5,44 @@ using UnityEngine;
 public class TileMovement : MonoBehaviour
 {
     public Vector3 targetPosition;
+    [SerializeField] private Transform empty;
+    private bool moving = false;
+    private bool cheating = false;
+    private SlidingPuzzle sp;
+
     void Start()
     {
         targetPosition = transform.position;
+        sp = GameObject.FindGameObjectWithTag("SlidingPuzzle").GetComponent<SlidingPuzzle>();
     }
 
     
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, targetPosition,0.05f);
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            cheating = true;
+        }
+        if (!cheating)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPosition, 0.05f);
+        }
+        if (Vector3.Distance(transform.position, targetPosition) < 1 && moving)
+        {
+            moving = false;
+        }
     }
+
+    public void checkvalid()
+    {
+        if (Vector3.Distance(transform.position, empty.position) < 370 && !moving)
+        {
+            sp.letTheSoundPlay();
+            moving = true;
+            Vector3 target = empty.position;
+            empty.position = transform.position;
+            targetPosition = target;
+        }
+    }
+
 }
