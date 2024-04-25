086002c7733ed140cd7d00f31f235556a0d4b47a
heidarsky <heidar078@gmail.com>
2024-04-25 06:14:40 -0500

    try add transition, few changes

--- Assets/Scripts/Mechanics/SceneChangeInvokable.cs
+++ Assets/Scripts/Mechanics/SceneChangeInvokable.cs
@@ -17,74 +17,42 @@ public class SceneChangeInvokable : MonoBehaviour, Invokable
 	public RawImage image;
 	//Video To Play [Assign from the Editor]
 	public VideoClip videoToPlay;
-	public VideoClip videoToEnd;
 
 	private VideoPlayer videoPlayer;
-	private VideoPlayer videoPlayer2;
 
 	private VideoSource videoSource;
 
+
 	IEnumerator ChangeScene()
 	{
-		//Add VideoPlayer to the GameObject
-		videoPlayer = gameObject.AddComponent<VideoPlayer>();
-		//Disable Play on Awake for Video
+		videoPlayer = GameObject.Find("GameManager").AddComponent<VideoPlayer>();
 		videoPlayer.playOnAwake = false;
 		videoPlayer.skipOnDrop = false;
-		//We want to play from video clip not from url
 		videoPlayer.source = VideoSource.VideoClip;
 		videoPlayer.clip = videoToPlay;
 		videoPlayer.Prepare();
-		//Wait until video is prepared
 		while (!videoPlayer.isPrepared)
 		{
 			yield return null;
 		}
-		//Assign the Texture from Video to RawImage to be displayed
 		image.texture = videoPlayer.texture;
 		videoPlayer.Play();
-		while (videoPlayer.isPlaying)
-		{
-			yield return null;
-		}
 
 		Debug.Log("Changing Scene");
 
 		var gm = GameManager.Instance;
 		gm.PlayDoorSound[sceneName] = IsDoor;
+		DontDestroyOnLoad(videoPlayer);
 
-		transitionAnim.SetTrigger("Start");
-		yield return new WaitForSeconds(1);
+		yield return new WaitForSeconds(1.35f);
 		Resources.UnloadUnusedAssets();
-
-		//Add VideoPlayer to the GameObject
-		Destroy(videoPlayer);
-		videoPlayer2 = gameObject.AddComponent<VideoPlayer>();
-		//Disable Play on Awake for Video
-		videoPlayer2.playOnAwake = false;
-		videoPlayer2.skipOnDrop = false;
-		//We want to play from video clip not from url
-		videoPlayer2.source = VideoSource.VideoClip;
-		videoPlayer2.clip = videoToEnd;
-		videoPlayer2.Prepare();
-		//Wait until video is prepared
-		while (!videoPlayer2.isPrepared)
-		{
-			yield return null;
-		}
-		//Assign the Texture from Video to RawImage to be displayed
-		image.texture = videoPlayer2.texture;
-		SceneManager.LoadScene(sceneName);
-		transitionAnim.SetTrigger("End");
-		videoPlayer2.Play();
-		while (videoPlayer2.isPlaying)
-		{
-			yield return null;
-		}
-		Destroy(videoPlayer);
-
+		AsyncOperation op = SceneManager.LoadSceneAsync(sceneName);
+		
+		Destroy(videoPlayer, 6);
 	}
-	public void Exit()
+	
+
+    public void Exit()
 	{
 		Application.Quit();
 	}
