0cb5b79eb6bd21ac28ee6bbc0a16f28af6ecfa64
Chris Saucedo <slapdaneck2003@gmail.com>
2024-04-27 15:36:23 -0500

    Added dialogue for the dragon paintings
    Need to effect the variables and get it to pop up.

---
+++ Assets/Scripts/dragonPaintingUnlock.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragonPaintingUnlock : MonoBehaviour
+{
+    public int paintingNum;
+
+    public bool hasSpokenToMain;
+    public bool hasGrabbedMini;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
