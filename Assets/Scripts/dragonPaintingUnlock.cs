a59bff4117697a51a420161bd2ce8080247271a7
Chris Saucedo <slapdaneck2003@gmail.com>
2024-04-28 20:40:53 -0500

    Added functionality to dialogue boxes for dragon

--- Assets/Scripts/dragonPaintingUnlock.cs
+++ Assets/Scripts/dragonPaintingUnlock.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class dragonPaintingUnlock : MonoBehaviour
 {
-    public GameManager manager;
     public DialogueUI dialogueUI;
 
     public DialogueObject smallFirst;
@@ -12,6 +11,7 @@ public class dragonPaintingUnlock : MonoBehaviour
 
     public DialogueObject BigFirst;
     public DialogueObject BigSecond;
+
 
 
     //public bool hasSpokenToMain = false;
@@ -27,70 +27,36 @@ public class dragonPaintingUnlock : MonoBehaviour
 
     public void checkSmallDragon() //when we talk to the big dragon
     {
-        if (manager.hasSpokeSmallDragon) //have we talked to small (1st floor)?
+        if (GameManager.Instance.hasSpokeSmallDragon) //have we talked to small (1st floor)?
         {
             dialogueUI.ShowDialogue(BigSecond);
         }
 
-        else dialogueUI.ShowDialogue(BigFirst); 
+        else
+        {
+            GameManager.Instance.setBigDragonStatus();
+            dialogueUI.ShowDialogue(BigFirst);
+        }
 
-    }
-    public void checkBigDragon() //when we talk to the small dragon
+        }
+        public void checkBigDragon() //when we talk to the small dragon
     {
-        if (manager.hasSpokeBigDragon) //if we have already talked to big
+        if (GameManager.Instance.hasSpokeBigDragon) //if we have already talked to big
         {
             miniPainting.SetActive(false);
             dialogueUI.ShowDialogue(smallSecond);
         }
 
-        else dialogueUI.ShowDialogue(smallFirst);
+        else 
+        {
+            GameManager.Instance.setSmallDragonStatus();
+            dialogueUI.ShowDialogue(smallFirst);
+        } 
+            
+            
 
 
     }
 
-    /*
-    public void CheckNewspaper()
-    {
-        List<string> items = gameManager.GetItmes();
-
-        hasNews1 = false;
-        hasNews2 = false;
-        hasNews3 = false;
-        for (int i = 0; i < items.Count; i++)
-        {
-            if (items[i] == "News1")
-            {
-                hasNews1 = true;
-            }
-            else if (items[i] == "News2")
-            {
-                hasNews2 = true;
-            }
-            else if (items[i] == "News3")
-            {
-                hasNews3 = true;
-            }
-        }
-        if (hasNews1 && hasNews2 && hasNews3)
-        {
-            condition = true;
-        }
-        else
-        {
-            condition = false;
-        }
-
-        if (condition)
-        {
-            DoorToOpen.CanEnter = true;
-            gameManager.OpenDoor();
-            Destroy(toDestroy);
-        }
-        else
-        {
-            dialogueUI.ShowDialogue(dialogueNo);
-        }
-
-    }
-    */
+    
 }
